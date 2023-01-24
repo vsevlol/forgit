5470ccbdfccc32ab6527157295e1c528b6596560
vsevlol <killura205@gmail.com>
2023-01-24 15:22:08 +0300

    nachinayu peredeliivat vvse uslovia c ne

---
+++ pkomandrec3j.py
@@ -0,0 +1,69 @@
+import pdb
+
+print('Введите натуральное число')
+chi1=input()
+chi=int(chi1)
+print('Введите несчастливое  число')
+ne1=input()
+ne=int(ne1)
+i=0
+slog3=[]
+k=0
+print('chi=',chi)
+def recur(chi):
+   global k
+   k=k+1
+   slog1=0
+   slog2=0
+   pdb.set_trace()
+               
+   
+   if chi%2==0:
+     slog1=chi/2
+     slog2=slog1
+     print('slog1,slog2 cht=',slog1,slog1)
+     if slog1==6 and ne==3:
+      slog3.append('4')
+      slog3.append('2')
+      slog3.append('4')
+      slog3.append('2')
+      i=1
+     if slog1==5 and ne==2:
+      slog3.append('5')
+      slog3.append('5')
+      i=1
+     if slog1==5 and ne==3:
+      slog3.append('5')
+      slog3.append('5')
+      i=1
+              
+     elif slog1+slog2==5:
+         if ne==3:
+          slog3.append(slog1+slog2)
+         elif ne==2:
+          slog3.append(slog1+slog2)
+       
+    
+       
+   else:
+     chi=chi+1
+     slog1=chi/2
+     slog2=slog1-1
+     #print('slog1,slog2  necht=',slog1,slog1)
+     if slog1>3: recur(slog1)
+     else:
+         slog3.append(slog1)
+         i=1
+     if slog2>3: recur(slog2)
+     else:
+        slog3.append(slog2)
+        i=1
+        
+     
+       
+         
+recur(chi)
+print(slog3)
+print('k=',k)
+slog4=slog3*4
+print(slog4)
